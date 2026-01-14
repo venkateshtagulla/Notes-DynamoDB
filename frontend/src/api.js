b1deffc603876af355a1cb4622f41c755db986bf
venkateshtagulla <154754274+venkateshtagulla@users.noreply.github.com>
2026-01-14 11:05:33 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:8000/api";
+const API_BASE_URL = "https://notes-dynamodb-v.onrender.com/api";
 
 export const createNote = async (title, content) => {
   const response = await fetch(`${API_BASE_URL}/notes`, {
